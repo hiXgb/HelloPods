b0ecd6d866f6d662561c63eb4d845bd60570d6de
xgb <xieguobi@xgb.local>
2017-08-22 11:58:45 +0800

    commit

---
+++ HelloPods.h
@@ -0,0 +1,13 @@
+//
+//  HelloPods.h
+//  TestXib
+//
+//  Created by xgb on 2017/8/22.
+//  Copyright © 2017年 xgb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HelloPods : NSObject
+
+@end
---
+++ HelloPods.m
@@ -0,0 +1,13 @@
+//
+//  HelloPods.m
+//  TestXib
+//
+//  Created by xgb on 2017/8/22.
+//  Copyright © 2017年 xgb. All rights reserved.
+//
+
+#import "HelloPods.h"
+
+@implementation HelloPods
+
+@end
